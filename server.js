fe5344f3178d79dd65c9c60573ad06c7c4ab9a11
ajhuff7 <70546019+ajhuff7@users.noreply.github.com>
2020-10-15 19:52:14 -0700

    server.js update

---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require("express");
+const { fstat } = require("fs");
+var path = require("path");
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+  res.json(jsonData);
+});
+
+app.post("/api/notes", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  var newNote = req.body;
+  jsonData.push(newNote);
+});
+
+fs.writeFile(db.json, JSON.stringify(jsonData))
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
